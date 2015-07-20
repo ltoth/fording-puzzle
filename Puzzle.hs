0d56e49de2e3e372d1d007fddf6a35b0875cfc77
Lukas Toth <lukas@lukastoth.com>
2015-07-20 08:08:59 -0400

    Change world model to include set of people on boat

--- Puzzle.hs
+++ Puzzle.hs
@@ -1,44 +1,62 @@
-{-# LANGUAGE LambdaCase #-}
 import Data.Set (Set)
 import qualified Data.Set as Set
+
+boatLimit = 2
 
 data Family = A | B | C deriving (Show, Read, Eq, Ord, Enum, Bounded)
 
 data Role = Father | Daughter deriving (Show, Read, Eq, Ord, Enum, Bounded)
 
-data Person = Person Role Family deriving (Show, Read, Eq, Ord)
+data Person = P Role Family deriving (Show, Read, Eq, Ord)
 
-data Boat = Near | Far deriving (Show, Read, Eq, Ord)
+data BoatLocation = Near | Far deriving (Show, Read, Eq, Ord)
+
+data Location = Shore BoatLocation | OnBoat deriving (Show, Read, Eq)
 
 type NearShore = Set Person
+type Boat = Set Person
 type FarShore = Set Person
 
-data World = World NearShore FarShore Boat deriving (Show, Read, Eq)
+data World = World NearShore Boat FarShore BoatLocation deriving (Show, Read, Eq, Ord)
 
-initialWorld = World allPeople
-                     Set.empty
-                     Near
+allPeople = [P r f | r <- enumAll, f <- enumAll]
 
-allPeople = Set.fromList [Person r f | r <- enumAll, f <- enumAll]
+initialWorld = World (Set.fromList allPeople) Set.empty Set.empty Near
 
 enumAll :: (Bounded a, Enum a) => [a]
 enumAll = [minBound .. maxBound]
 
 isDaughterOf :: Person -> Person -> Bool
-(Person Daughter f1) `isDaughterOf` (Person Father f2) = f1 == f2
+(P Daughter f1) `isDaughterOf` (P Father f2) = f1 == f2
 _ `isDaughterOf` _ = False
 
-legalMoves :: World -> [World -> World]
-legalMoves w@(World n f b) = [move (Set.singleton s) | s <- Set.toList (allPeopleOnShore b w)]
-                         -- ++ [other legal moves]
+legalMoves :: World -> Set World
+legalMoves w = Set.filter isWorldLegal worlds
+  where worlds = Set.fromList $ moveBoat w : [move p w | p <- allPeople]
 
-allPeopleOnShore :: Boat -> World -> Set Person
-allPeopleOnShore Near (World n f _) = n
-allPeopleOnShore Far  (World n f _) = f
+locatePerson :: Person -> World -> Location
+locatePerson p (World n _ f _) | p `Set.member` n = Shore Near
+                               | p `Set.member` f = Shore Far
+                               | otherwise        = OnBoat
 
-move :: Set Person -> World -> World
-move s = \case
-            (World n f Near) -> World (Set.difference n s') (Set.union f s') Far
-              where s' = Set.intersection n s
-            (World n f Far)  -> World (Set.union n s') (Set.difference f s') Near
-              where s' = Set.intersection f s
+move :: Person -> World -> World
+move p w@(World n b f Near) =
+  case locatePerson p w of
+    Shore Near -> World (Set.delete p n) (Set.insert p b) f Near
+    OnBoat     -> World (Set.insert p n) (Set.delete p b) f Near
+    Shore Far  -> w  -- impossible; person is on different shore from boat
+move p w@(World n b f Far) =
+  case locatePerson p w of
+    Shore Near -> w  -- impossible; person is on different shore from boat
+    OnBoat     -> World n (Set.delete p b) (Set.insert p f) Far
+    Shore Far  -> World n (Set.insert p b) (Set.delete p f) Far
+
+moveBoat :: World -> World
+moveBoat w@(World n b f Near) = if Set.size b > 0 then World n b f Far else w
+moveBoat w@(World n b f Far)  = if Set.size b > 0 then World n b f Near else w
+
+isWorldLegal :: World -> Bool
+isWorldLegal (World n b f l)
+  | Set.size b > boatLimit = False
+  -- | 
+  | otherwise = True
